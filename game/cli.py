454894b4613291cccfee282f0eb22388291b58f9
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-08-22 20:08:28 -0300

    3rd change in cli

--- game/cli.py
+++ game/cli.py
@@ -1,13 +1,15 @@
 from game.class_chess import Chess
 
+
 def main():
     chess = Chess()
     while chess.is_playing():
         play(chess)
 
+
 def play(chess):
     try:
-        # print(chess.show_board())
+        print(chess.show_board())
         print("turn: ", chess.turn)
         from_row = int(input("From row: "))
         from_col = int(input("From col: "))
@@ -20,18 +22,10 @@ def play(chess):
             to_row,
             to_col,
         )
-        print("Move successful!")
     except Exception as e:
         print("error", e)
 
-def show_instructions():
-    print("Welcome to Chess!")
-    print("Instructions:")
-    print("1. Enter the row and column of the piece you want to move.")
-    print("2. Enter the row and column of the destination.")
-    print("3. Follow the prompts to make your move.")
-    print("4. Type 'exit' to quit the game.")
+
 
 if __name__ == '__main__':
-    show_instructions()
     main()
