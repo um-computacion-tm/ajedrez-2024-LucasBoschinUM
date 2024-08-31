e450c30fe5afcc6fad479394e625fac79ff966e0
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-08-30 21:08:14 -0300

    3rd commit on Test_Pieces

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -1,10 +1,12 @@
 import unittest
 from game.class_pieces import Piece
+from game.class_board import Board  # Asegúrate de importar la clase Board
 
 class TestPiece(unittest.TestCase):
     def test_initialization(self):
         color = "white"
-        piece = Piece(color)
+        board = Board()  # Crear una instancia de Board
+        piece = Piece(color, board)  # Pasar color y board al constructor
         self.assertEqual(piece.__color__, color)
 
 if __name__ == '__main__':
