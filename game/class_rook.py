cc4c2e30725f5f15aa88d89090a2988307293410
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-08-31 21:58:46 -0300

    4th commit on class_rook

--- game/class_rook.py
+++ game/class_rook.py
@@ -1,11 +1,37 @@
 from game.class_pieces import Piece
 
 class Rook(Piece):
-    def __init__(self, __color__):
-        super().__init__(__color__)
-    
-    def __str__(self):
-        if self.__color__ == "WHITE":
-            return "♜"
-        else:
-            return "♖"
+    white_str = "♜"
+    black_str = "♖"
+
+    def valid_positions(
+        self,
+        from_row,
+        from_col,
+        to_row,
+        to_col,
+    ):
+        possible_positions = (
+            self.possible_positions_vd(from_row, from_col) +
+            self.possible_positions_va(from_row, from_col)
+        )
+        return (to_row, to_col) in possible_positions
+
+    def possible_positions_vd(self, row, col):
+        possibles = []
+        for next_row in range(row + 1, 8):
+            # que la celda que sigue no este ocupada..
+            other_piece = self.__board__.get_piece(next_row, col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((next_row, col))
+                break
+            possibles.append((next_row, col))
+        return possibles
+
+    def possible_positions_va(self, row, col):
+        possibles = []
+        for next_row in range(row - 1, -1, -1):
+            possibles.append((next_row, col))
+        return possibles
+
