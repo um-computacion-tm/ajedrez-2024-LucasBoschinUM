b33ca01b6f4c726729c886ac9014f8769794c1ca
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-09-02 08:50:52 -0300

    3rd commit on class_board

--- game/class_board.py
+++ game/class_board.py
@@ -1,18 +1,17 @@
 from game.class_rook import Rook
 
-
 class Board:
-    def __init__(self):
+    def __init__(self, for_test=False):
         self.__positions__ = []
         for _ in range(8):
             col = []
             for _ in range(8):
                 col.append(None)
             self.__positions__.append(col)
-        self.__positions__[0][0] = Rook("BLACK") # Black
-        self.__positions__[0][7] = Rook("BLACK") # Black
-        self.__positions__[7][7] = Rook("WHITE") # White
-        self.__positions__[7][0] = Rook("WHITE") # White
+        self.__positions__[0][0] = Rook("BLACK", self) # Black
+        self.__positions__[0][7] = Rook("BLACK", self) # Black
+        self.__positions__[7][7] = Rook("WHITE", self) # White
+        self.__positions__[7][0] = Rook("WHITE", self) # White
 
     def __str__(self):
         board_str = ""
@@ -27,3 +26,6 @@ class Board:
 
     def get_piece(self, row, col):
         return self.__positions__[row][col]
+    
+    def set_piece(self, row, col, piece):
+        self.__positions__[row][col] = piece
