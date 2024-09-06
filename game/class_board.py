c7b28d4d9b6ca65c92b5c6f012fc77abde60512c
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-09-06 17:38:46 -0300

    4th commit on class_board

--- game/class_board.py
+++ game/class_board.py
@@ -1,17 +1,20 @@
 from game.class_rook import Rook
+from game.exceptions import OutOfBoard
+
 
 class Board:
-    def __init__(self, for_test=False):
+    def __init__(self, for_test = False):
         self.__positions__ = []
         for _ in range(8):
             col = []
             for _ in range(8):
                 col.append(None)
             self.__positions__.append(col)
-        self.__positions__[0][0] = Rook("BLACK", self) # Black
-        self.__positions__[0][7] = Rook("BLACK", self) # Black
-        self.__positions__[7][7] = Rook("WHITE", self) # White
-        self.__positions__[7][0] = Rook("WHITE", self) # White
+        if not for_test:
+            self.__positions__[0][0] = Rook("BLACK", self) # Black
+            self.__positions__[0][7] = Rook("BLACK", self) # Black
+            self.__positions__[7][7] = Rook("WHITE", self) # White
+            self.__positions__[7][0] = Rook("WHITE", self) # White
 
     def __str__(self):
         board_str = ""
@@ -25,7 +28,16 @@ class Board:
         return board_str
 
     def get_piece(self, row, col):
+        if not (
+            0 <= row < 8 or 0 <= col < 8
+        ):
+            raise OutOfBoard()
         return self.__positions__[row][col]
-    
+
     def set_piece(self, row, col, piece):
         self.__positions__[row][col] = piece
+
+    def move(self, from_row, from_col, to_row, to_col):
+        origin = self.get_piece(from_row, from_col)
+        self.set_piece(to_row, to_col, origin)
+        self.set_piece(from_row, from_col, None)
