e2b22caff3e43ce46e645f8187e32302688ccb5c
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-09-11 22:14:13 -0300

    5th commit on class_chess

--- game/class_chess.py
+++ game/class_chess.py
@@ -1,5 +1,5 @@
 from game.class_board import Board
-from game.exceptions import InvalidMove
+from game.exceptions import InvalidMove, InvalidTurn, EmptyPosition
 
 class Chess:
     def __init__(self):
@@ -18,9 +18,15 @@ class Chess:
     ):
         # validate coords
         piece = self.__board__.get_piece(from_row, from_col)
-        if piece.valid_positions(from_row, from_col, to_row, to_col):
+        if not piece:
+            raise EmptyPosition()
+        if not piece.get_color() == self.__turn__:
+            raise InvalidTurn()
+        if not piece.valid_positions(from_row, from_col, to_row, to_col):
             raise InvalidMove()
+        self.__board__.move(from_row, from_col, to_row, to_col)
         self.change_turn()
+
     @property
     def turn(self):
         return self.__turn__
