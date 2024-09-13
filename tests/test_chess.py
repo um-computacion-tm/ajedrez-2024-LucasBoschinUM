edfc0c4f4daf3a2d49d0c5275fb236542c1a4bfd
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-09-13 12:23:05 -0300

    3rd commit on test_chess

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -1,6 +1,7 @@
 import unittest
 from game.class_chess import Chess
 from game.class_board import Board
+from game.class_rook import Rook
 
 class TestChess(unittest.TestCase):
     def setUp(self):
@@ -16,11 +17,12 @@ class TestChess(unittest.TestCase):
         self.chess.change_turn()
         self.assertEqual(self.chess.turn, "WHITE")
 
-    def test_move_changes_turn(self):
-        # Mock the board's get_piece method
-        self.chess._Chess__board__.get_piece = lambda x, y: "Pawn"
-        self.chess.move(0, 0, 1, 1)
-        self.assertEqual(self.chess.turn, "BLACK")
+def test_move_changes_turn(self):
+    # Mock the board's get_piece method to return a white piece
+    self.chess._Chess__board__.get_piece = lambda x, y: Rook("WHITE", self.chess._Chess__board__)
+    self.chess._Chess__board__.set_piece(7, 0, Rook("WHITE", self.chess._Chess__board__))  # Set a white rook at (7, 0)
+    self.chess.move(7, 0, 6, 0)  # Move the white rook from (7, 0) to (6, 0)
+    self.assertEqual(self.chess.turn, "BLACK")
 
 if __name__ == '__main__':
     unittest.main()
