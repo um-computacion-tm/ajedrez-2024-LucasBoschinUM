0870f318b7156e7ba1bfd6be82dcbbfd47c216a9
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-09-13 12:23:43 -0300

    2nd commit on test_pawn

--- tests/test_pawn.py
+++ tests/test_pawn.py
@@ -3,11 +3,11 @@ from game.class_pawn import Pawn
 
 class TestPawn(unittest.TestCase):
     def test_white_str(self):
-        pawn = Pawn()
+        pawn = Pawn(color="white", board=None)  # Proporcionar los argumentos necesarios
         self.assertEqual(pawn.white_str, "♜")
 
     def test_black_str(self):
-        pawn = Pawn()
+        pawn = Pawn(color="black", board=None)  # Proporcionar los argumentos necesarios
         self.assertEqual(pawn.black_str, "♖")
 
 if __name__ == '__main__':
