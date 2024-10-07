8caaa60c181250087104b4b9a37d26fcac4308d4
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-10-06 22:09:32 -0300

    2nd commit on class_bishop

--- game/class_bishop.py
+++ game/class_bishop.py
@@ -9,3 +9,31 @@ class Bishop(Piece):
             from_row,
             from_col,
         )
+
+    #def possible_diagonal_positions(self, from_row, from_col):
+        positions = []
+        # Diagonal superior derecha
+        for i in range(1, 8):
+            if from_row + i < 8 and from_col + i < 8:
+                positions.append((from_row + i, from_col + i))
+            else:
+                break
+        # Diagonal superior izquierda
+        for i in range(1, 8):
+            if from_row + i < 8 and from_col - i >= 0:
+                positions.append((from_row + i, from_col - i))
+            else:
+                break
+        # Diagonal inferior derecha
+        for i in range(1, 8):
+            if from_row - i >= 0 and from_col + i < 8:
+                positions.append((from_row - i, from_col + i))
+            else:
+                break
+        # Diagonal inferior izquierda
+        for i in range(1, 8):
+            if from_row - i >= 0 and from_col - i >= 0:
+                positions.append((from_row - i, from_col - i))
+            else:
+                break
+        return positions
