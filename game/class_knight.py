5448f72c834837c3dc76120bb3f9441607d74ba3
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-10-08 23:51:33 -0300

    2nd commit on class_knight

--- game/class_knight.py
+++ game/class_knight.py
@@ -13,6 +13,14 @@ class Knight(Piece):
         for move in moves:
             new_row = from_row + move[0]
             new_col = from_col + move[1]
-            if 0 <= new_row < 8 and 0 <= new_col < 8:
+            if self.is_valid_move(new_row, new_col):
                 positions.append((new_row, new_col))
         return positions
+
+    def is_valid_move(self, row, col):
+        if not (0 <= row < 8 and 0 <= col < 8):
+            return False
+        destination_piece = self.__board__.get_piece(row, col)
+        if destination_piece is not None and destination_piece.get_color() == self.get_color():
+            return False
+        return True
