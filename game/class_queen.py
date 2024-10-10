fb9d4e31df83a206b1b32fac9c263e8ab51ddf72
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-10-10 18:12:33 -0300

    3rd commit on class_queen

--- game/class_queen.py
+++ game/class_queen.py
@@ -60,3 +60,31 @@ class Queen(Piece):
                 break
             possibles.append((from_row, from_col - i))
         return possibles
+
+    def is_valid_move(self, from_row, from_col, to_row, to_col):
+        if not (0 <= to_row < 8 and 0 <= to_col < 8):
+            return False
+        destination_piece = self.__board__.get_piece(to_row, to_col)
+        if destination_piece and destination_piece.get_color() == self.get_color():
+            return False
+        if from_row == to_row:
+            step = 1 if to_col > from_col else -1
+            for col in range(from_col + step, to_col, step):
+                if self.__board__.get_piece(from_row, col):
+                    return False
+        elif from_col == to_col:
+            step = 1 if to_row > from_row else -1
+            for row in range(from_row + step, to_row, step):
+                if self.__board__.get_piece(row, from_col):
+                    return False
+        elif abs(from_row - to_row) == abs(from_col - to_col):
+            row_step = 1 if to_row > from_row else -1
+            col_step = 1 if to_col > from_col else -1
+            for i in range(1, abs(from_row - to_row)):
+                if self.__board__.get_piece(from_row + i * row_step, from_col + i * col_step):
+                    return False
+        return True
+
+    def get_valid_moves(self, from_row, from_col):
+        possible_positions = self.get_possible_positions(from_row, from_col)
+        return [(to_row, to_col) for to_row, to_col in possible_positions if self.is_valid_move(from_row, from_col, to_row, to_col)]
