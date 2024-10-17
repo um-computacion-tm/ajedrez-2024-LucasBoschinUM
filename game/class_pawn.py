9e319c50c5218b1e97705081b4d47f3b9d044692
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-10-17 20:55:20 -0300

    7th commit on class_pawn

--- game/class_pawn.py
+++ game/class_pawn.py
@@ -15,12 +15,26 @@ class Pawn(Piece):
         return possibles
 
     def get_possible_positions_eat(self, from_row, from_col):
+        possible_positions = []
         if self.__color__ == "BLACK":
-            other_piece = self.__board__.get_piece(from_row + 1, from_col + 1)
-            if other_piece and other_piece.__color__ == "WHITE":
-                return [(from_row + 1, from_col + 1)]
-
-        return []
+            if from_col > 0:
+                other_piece = self.__board__.get_piece(from_row + 1, from_col - 1)
+                if other_piece and other_piece.__color__ == "WHITE":
+                    possible_positions.append((from_row + 1, from_col - 1))
+            if from_col < 7:
+                other_piece = self.__board__.get_piece(from_row + 1, from_col + 1)
+                if other_piece and other_piece.__color__ == "WHITE":
+                    possible_positions.append((from_row + 1, from_col + 1))
+        else:
+            if from_col > 0:
+                other_piece = self.__board__.get_piece(from_row - 1, from_col - 1)
+                if other_piece and other_piece.__color__ == "BLACK":
+                    possible_positions.append((from_row - 1, from_col - 1))
+            if from_col < 7:
+                other_piece = self.__board__.get_piece(from_row - 1, from_col + 1)
+                if other_piece and other_piece.__color__ == "BLACK":
+                    possible_positions.append((from_row - 1, from_col + 1))
+        return possible_positions
 
     def get_possible_positions_move(self, from_row, from_col):
         possible_positions = []
@@ -31,9 +45,9 @@ class Pawn(Piece):
                 if from_row == 1 and self.__board__.get_piece(from_row + 2, from_col) is None:
                     possible_positions.append((from_row + 2, from_col))
         else:
-            if from_row == 6:
-                possible_positions.extend([(from_row - 1, from_col), (from_row - 2, from_col)])
-            elif self.__board__.get_piece(from_row - 1, from_col) is None:
+            if self.__board__.get_piece(from_row - 1, from_col) is None:
                 possible_positions.append((from_row - 1, from_col))
+                if from_row == 6 and self.__board__.get_piece(from_row - 2, from_col) is None:
+                    possible_positions.append((from_row - 2, from_col))
 
         return possible_positions
