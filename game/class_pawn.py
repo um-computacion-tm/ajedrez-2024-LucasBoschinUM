bd8d9e9f406e3351bc17403bbe3d59744e2e9c28
LucasBoschinUM <l.boschin@alumno.um.edu.ar>
2024-10-21 21:35:58 -0300

    Fixing issues to get an A in Maintainability 7

--- game/class_pawn.py
+++ game/class_pawn.py
@@ -9,41 +9,23 @@ class Pawn(Piece):
         possibles.extend(self.get_possible_positions_eat(from_row, from_col))
         return possibles
 
-    def get_capture_positions(self, from_row, from_col, row_offset, col_offset, opponent_color):
-        possible_positions = []
-        new_row = from_row + row_offset
-        new_col = from_col + col_offset
-        if 0 <= new_col < 8:
-            other_piece = self.__board__.get_piece(new_row, new_col)
-            if other_piece and other_piece.__color__ == opponent_color:
-                possible_positions.append((new_row, new_col))
-        return possible_positions
-
-    def get_capture_positions_for_color(self, from_row, from_col, offsets, opponent_color):
-        possible_positions = []
-        possible_positions.extend(self.get_capture_positions(from_row, from_col, offsets['row_offset'], offsets['col_offset1'], opponent_color))
-        possible_positions.extend(self.get_capture_positions(from_row, from_col, offsets['row_offset'], offsets['col_offset2'], opponent_color))
-        return possible_positions
-
-    def get_capture_positions_with_offsets(self, from_row, from_col, offsets, opponent_color):
-        return self.get_capture_positions_for_color(from_row, from_col, offsets, opponent_color)
-
-    def get_capture_positions_generic(self, from_row, from_col, offsets, opponent_color):
-        return self.get_capture_positions_with_offsets(from_row, from_col, offsets, opponent_color)
-
-    def get_black_capture_positions(self, from_row, from_col):
-        offsets = {'row_offset': 1, 'col_offset1': -1, 'col_offset2': 1}
-        return self.get_capture_positions_generic(from_row, from_col, offsets, "WHITE")
-
-    def get_white_capture_positions(self, from_row, from_col):
-        offsets = {'row_offset': -1, 'col_offset1': -1, 'col_offset2': 1}
-        return self.get_capture_positions_generic(from_row, from_col, offsets, "BLACK")
-
     def get_possible_positions_eat(self, from_row, from_col):
+        def add_position_if_valid(row_offset, col_offset, target_color):
+            new_row, new_col = from_row + row_offset, from_col + col_offset
+            if 0 <= new_col <= 7:
+                other_piece = self.__board__.get_piece(new_row, new_col)
+                if other_piece and other_piece.__color__ == target_color:
+                    possible_positions.append((new_row, new_col))
+
+        possible_positions = []
         if self.__color__ == "BLACK":
-            return self.get_black_capture_positions(from_row, from_col)
+            add_position_if_valid(1, -1, "WHITE")
+            add_position_if_valid(1, 1, "WHITE")
         else:
-            return self.get_white_capture_positions(from_row, from_col)
+            add_position_if_valid(-1, -1, "BLACK")
+            add_position_if_valid(-1, 1, "BLACK")
+
+        return possible_positions
 
     def get_possible_positions_move(self, from_row, from_col):
         possible_positions = []
